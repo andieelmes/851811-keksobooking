bd06528f6bf5a90703da6fbfb4d610420628bd0c
z.salikhkulova <z.salikhkulova@fun-box.ru>
2018-12-13 15:39:38 +0500

    remove old consts

--- js/consts.js
+++ js/consts.js
@@ -4,12 +4,6 @@
 (function () {
   var NUMBER_OF_LISTINGS = 5;
 
-  var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var TIMES = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
   var CARD_ELEMENT_CLASS = '.map__card';
@@ -59,11 +53,6 @@
 
   window.consts = {
     NUMBER_OF_LISTINGS: NUMBER_OF_LISTINGS,
-    TITLES: TITLES,
-    TYPES: TYPES,
-    TIMES: TIMES,
-    FEATURES: FEATURES,
-    PHOTOS: PHOTOS,
     ESC_KEYCODE: ESC_KEYCODE,
     ENTER_KEYCODE: ENTER_KEYCODE,
     CARD_ELEMENT_CLASS: CARD_ELEMENT_CLASS,
