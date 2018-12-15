5b6e8cdc794979e1c1ee8339a706060f6b5f26f2
z.salikhkulova <z.salikhkulova@fun-box.ru>
2018-12-15 13:56:12 +0500

    add photo upload

---
+++ js/photoUpload.js
@@ -0,0 +1,51 @@
+'use strict';
+(function () {
+  var uploadPhoto = function (input, preview, multiple) {
+    if (multiple) {
+      var uploaded = false;
+      var previewTemplate = preview.cloneNode(true);
+    }
+
+    var onFileInputChange = function () {
+      Array.prototype.forEach.call(input.files, function (file) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          if (multiple) {
+            var photo = document.createElement('img');
+            photo.src = reader.result;
+            photo.style.maxWidth = '100%';
+            photo.style.display = 'block';
+
+            if (uploaded) {
+              var parent = preview.parentElement;
+              var nextPreview = previewTemplate.cloneNode(true);
+              nextPreview.appendChild(photo);
+              parent.appendChild(nextPreview);
+            } else {
+              preview.appendChild(photo);
+              uploaded = true;
+            }
+          } else {
+            preview.src = reader.result;
+          }
+        });
+
+        reader.readAsDataURL(file);
+      });
+    };
+
+    input.addEventListener('change', onFileInputChange);
+  };
+
+  var avatarInputElement = document.querySelector('.ad-form__field input[type=file]');
+  var avatarElement = document.querySelector('.ad-form-header__preview img');
+
+  uploadPhoto(avatarInputElement, avatarElement);
+
+  var photoInputElement = document.querySelector('.ad-form__upload input[type=file]');
+  var photoElement = document.querySelector('.ad-form__photo');
+
+  uploadPhoto(photoInputElement, photoElement, true);
+
+})();
