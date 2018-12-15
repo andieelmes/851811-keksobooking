1e705f564ae2016f6aa03ea4b0ed3c72cf8e14ad
z.salikhkulova <z.salikhkulova@fun-box.ru>
2018-12-15 14:22:47 +0500

    add active class to the pin

--- js/setup.js
+++ js/setup.js
@@ -13,12 +13,16 @@
       var address = listing.location.x.toString() + ', ' + listing.location.y.toString();
       var pin = window.vars.mapPinsElement.querySelector('[data-address="' + address + '"]');
       pin.addEventListener('click', function () {
-        makeMapCardElement(listing);
+        makeMapCardElement(listing, pin);
+        window.vars.mapPinsElement.querySelectorAll('.map__pin').forEach(function (item) {
+          item.classList.remove('map__pin--active');
+        });
+        pin.classList.add('map__pin--active');
       });
     });
   };
 
-  var makeMapCardElement = function (listing) {
+  var makeMapCardElement = function (listing, pin) {
     var cardElement = window.vars.mapElement.querySelector(window.consts.CARD_ELEMENT_CLASS);
     if (cardElement) {
       cardElement.remove();
@@ -30,15 +34,18 @@
 
     document.addEventListener('keydown', function (e) {
       window.utils.onPopupEscPress(e, card);
+      pin.classList.remove('map__pin--active');
     });
 
     cardCloseElement.addEventListener('click', function () {
       window.utils.closePopup(card);
+      pin.classList.remove('map__pin--active');
     });
 
     cardCloseElement.addEventListener('keydown', function (e) {
       if (e.keyCode === window.consts.ENTER_KEYCODE) {
         window.utils.closePopup(card);
+        pin.classList.remove('map__pin--active');
       }
     });
   };
